8147edd444745eddeed9dd0166be3e2ae2ba277e
Steve Kadlec <steven.kadlec@ga.com>
2024-03-26 15:11:59 -0600

    targets.cmake applist change because sbn_* modules moved.

---
+++ apps/sbn_f_remap/fsw/src/sbn_f_remap.c
@@ -0,0 +1,324 @@
+#include "sbn_interfaces.h"
+#include "sbn_remap_tbl.h"
+#include "sbn_msgids.h"
+#include "cfe.h"
+#include "cfe_tbl.h"
+#include <string.h> /* memcpy */
+#include <stdlib.h> /* qsort */
+
+#include "sbn_f_remap_events.h"
+
+const char SBN_F_REMAP_TABLE_NAME[] = "SBN_RemapTbl";
+
+bool             RemapInitialized = false;
+OS_MutexID_t     RemapMutex  = 0;
+CFE_TBL_Handle_t RemapTblHandle = 0;
+SBN_RemapTbl_t   *RemapTbl    = NULL;
+int              RemapTblCnt = 0;
+
+CFE_EVS_EventID_t SBN_F_REMAP_FIRST_EID;
+
+static int RemapTblVal(void *TblPtr)
+{
+    SBN_RemapTbl_t *r = (SBN_RemapTbl_t *)TblPtr;
+    int             i = 0;
+
+    switch (r->RemapDefaultFlag)
+    {
+        /* all valid values */
+        case SBN_REMAP_DEFAULT_IGNORE:
+        case SBN_REMAP_DEFAULT_SEND:
+            break;
+        /* otherwise, unknown! */
+        default:
+            return -1;
+    } /* end switch */
+
+    /* Find the first "empty" entry (with a 0x0000 "from") to determine table
+     * size.
+     */
+    for (i = 0; i < SBN_REMAP_TABLE_SIZE; i++)
+    {
+        if (CFE_SB_MsgId_Equal(r->Entries[i].FromMID, CFE_SB_ValueToMsgId(0x0000)))
+        {
+            break;
+        } /* end if */
+    }     /* end for */
+
+    RemapTblCnt = i;
+
+    return 0;
+} /* end RemapTblVal() */
+
+static int RemapTblCompar(const void *a, const void *b)
+{
+    SBN_RemapTblEntry_t *aEntry = (SBN_RemapTblEntry_t *)a;
+    SBN_RemapTblEntry_t *bEntry = (SBN_RemapTblEntry_t *)b;
+
+    if (aEntry->SpacecraftID != bEntry->SpacecraftID)
+    {
+        return aEntry->SpacecraftID - bEntry->SpacecraftID;
+    }
+    if (aEntry->ProcessorID != bEntry->ProcessorID)
+    {
+        return aEntry->ProcessorID - bEntry->ProcessorID;
+    }
+    return CFE_SB_MsgIdToValue(aEntry->FromMID) - CFE_SB_MsgIdToValue(bEntry->FromMID);
+} /* end RemapTblCompar() */
+
+static SBN_Status_t LoadRemapTbl(void)
+{
+    SBN_RemapTbl_t * TblPtr         = NULL;
+    CFE_Status_t     CFE_Status     = CFE_SUCCESS;
+
+    if (CFE_TBL_Register(&RemapTblHandle, SBN_F_REMAP_TABLE_NAME, sizeof(SBN_RemapTbl_t), CFE_TBL_OPT_DEFAULT, &RemapTblVal) !=
+        CFE_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_TBL_EID, "unable to register remap tbl handle");
+        return SBN_ERROR;
+    } /* end if */
+
+    if (CFE_TBL_Load(RemapTblHandle, CFE_TBL_SRC_FILE, SBN_REMAP_TBL_FILENAME) != CFE_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_TBL_EID, "unable to load remap tbl %s", SBN_REMAP_TBL_FILENAME);
+        CFE_TBL_Unregister(RemapTblHandle);
+        return SBN_ERROR;
+    } /* end if */
+
+    EVSSendDbg(SBN_F_REMAP_TBL_EID, "remap table loaded");
+
+    if ((CFE_Status = CFE_TBL_GetAddress((void **)&TblPtr, RemapTblHandle)) != CFE_TBL_INFO_UPDATED)
+    {
+        EVSSendErr(SBN_F_REMAP_TBL_EID, "unable to get conf table address");
+        CFE_TBL_Unregister(RemapTblHandle);
+        return SBN_ERROR;
+    } /* end if */
+
+    EVSSendDbg(SBN_F_REMAP_TBL_EID, "remap table address retrieved");
+    /* sort the entries on <ProcessorID> and <from MID> */
+    /* note: qsort is recursive, so it will use some stack space
+     * (O[N log N] * <some small amount of stack>). If this is a concern,
+     * consider using a non-recursive (insertion, bubble, etc) sort algorithm.
+     */
+
+    qsort(TblPtr->Entries, RemapTblCnt, sizeof(SBN_RemapTblEntry_t), RemapTblCompar);
+    EVSSendDbg(SBN_F_REMAP_TBL_EID, "remap table sorted");
+
+    CFE_TBL_Modified(RemapTblHandle);
+
+    RemapTbl = TblPtr;
+
+    return SBN_SUCCESS;
+} /* end LoadRemapTbl() */
+
+/* finds the entry or the one that would immediately follow it */
+static int BinarySearch(void *Entries, void *SearchEntry, size_t EntryCnt, size_t EntrySz,
+                        int (*EntryCompare)(const void *, const void *))
+{
+    int start, end, midpoint, found;
+
+    for (start = 0, end = EntryCnt - 1, found = 0; found == 0 && start <= end;)
+    {
+        midpoint = (end + start) / 2;
+        int c    = EntryCompare(SearchEntry, (uint8 *)Entries + EntrySz * midpoint);
+        if (c == 0)
+        {
+            return midpoint;
+        }
+        else if (c > 0)
+        {
+            start = midpoint + 1;
+        }
+        else
+        {
+            end = midpoint - 1;
+        } /* end if */
+    }     /* end while */
+
+    if (found == 0)
+    {
+        return EntryCnt;
+    } /* end if */
+
+    return midpoint;
+} /* end BinarySearch() */
+
+static int RemapTblSearch(uint32 ProcessorID, uint32 SpacecraftID, CFE_SB_MsgId_t MID)
+{
+    SBN_RemapTblEntry_t Entry = {ProcessorID, SpacecraftID, MID, {0}};
+    return BinarySearch(RemapTbl->Entries, &Entry, RemapTblCnt, sizeof(SBN_RemapTblEntry_t), RemapTblCompar);
+} /* end RemapTblSearch() */
+
+static SBN_Status_t Remap(void *msg, SBN_Filter_Ctx_t *Context)
+{
+    CFE_SB_MsgId_t     FromMID = CFE_SB_ValueToMsgId(0x0000), ToMID = CFE_SB_ValueToMsgId(0x0000);
+    CFE_MSG_Message_t *CFE_MsgPtr = msg;
+
+    if (CFE_MSG_GetMsgId(CFE_MsgPtr, &FromMID) != CFE_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_EID, "unable to get apid");
+        return SBN_ERROR;
+    } /* end if */
+
+    if (OS_MutSemTake(RemapMutex) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_EID, "unable to take mutex");
+        return SBN_ERROR;
+    } /* end if */
+
+    int i = RemapTblSearch(Context->PeerProcessorID, Context->PeerSpacecraftID, FromMID);
+
+    if (i < RemapTblCnt &&
+        RemapTbl->Entries[i].ProcessorID == Context->PeerProcessorID &&
+        RemapTbl->Entries[i].SpacecraftID == Context->PeerSpacecraftID &&
+        CFE_SB_MsgId_Equal(RemapTbl->Entries[i].FromMID, FromMID))
+    {
+        ToMID = RemapTbl->Entries[i].ToMID;
+    }
+    else
+    {
+        if (RemapTbl->RemapDefaultFlag == SBN_REMAP_DEFAULT_SEND)
+        {
+            ToMID = FromMID;
+        } /* end if */
+    }     /* end if */
+
+    if (OS_MutSemGive(RemapMutex) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_EID, "unable to give mutex");
+        return SBN_ERROR;
+    } /* end if */
+
+    if (CFE_SB_MsgId_Equal(ToMID, CFE_SB_ValueToMsgId(0x0000)))
+    {
+        return SBN_IF_EMPTY; /* signal to the core app that this filter recommends not sending this message */
+    }                        /* end if */
+
+    if (CFE_MSG_SetMsgId(CFE_MsgPtr, ToMID) != CFE_SUCCESS)
+    {
+        EVSSendErr(SBN_F_REMAP_EID, "unable to set msgid");
+        return SBN_ERROR;
+    } /* end if */
+
+    return SBN_SUCCESS;
+} /* end Remap() */
+
+static SBN_Status_t Remap_MID(CFE_SB_MsgId_t *InOutMsgIdPtr, SBN_Filter_Ctx_t *Context)
+{
+    int i = 0;
+
+    EVSSendDbg(SBN_F_REMAP_TBL_EID, "Remap check 0x%04X", CFE_SB_MsgIdToValue(*InOutMsgIdPtr));
+    for (i = 0; i < RemapTblCnt; i++)
+    {
+        if (RemapTbl->Entries[i].ProcessorID == Context->PeerProcessorID &&
+            RemapTbl->Entries[i].SpacecraftID == Context->PeerSpacecraftID &&
+            CFE_SB_MsgId_Equal(RemapTbl->Entries[i].ToMID, *InOutMsgIdPtr))
+        {
+            *InOutMsgIdPtr = RemapTbl->Entries[i].FromMID;
+            EVSSendDbg(SBN_F_REMAP_TBL_EID, " --> 0x%04X", CFE_SB_MsgIdToValue(*InOutMsgIdPtr));
+            return SBN_SUCCESS;
+        } /* end if */
+    }     /* end for */
+
+    return SBN_SUCCESS;
+} /* end Remap_MID() */
+
+static SBN_Status_t UnloadRemapTbl(void)
+{
+    //  Check the handle to a previously-registered table
+    if (CFE_TBL_GetStatus(RemapTblHandle) == CFE_SUCCESS)
+    {
+        // Unregister and free resources used by table
+        if(CFE_TBL_Unregister(RemapTblHandle) != CFE_SUCCESS) {
+            EVSSendErr(SBN_F_REMAP_TBL_EID, "unable to unregister table");
+        }
+        RemapTbl = NULL;
+    } else {
+        EVSSendErr(SBN_F_REMAP_TBL_EID, "unable to get registered table");
+        return SBN_ERROR;
+    }
+
+    return SBN_SUCCESS;
+}
+
+static SBN_Status_t Deinit(CFE_EVS_EventID_t BaseEID)
+{
+    if (OS_MutSemTake(RemapMutex) != OS_SUCCESS)
+    {
+        EVSSendErr(BaseEID, "unable to take mutex");
+        return SBN_ERROR;
+    } /* end if */
+
+    if(UnloadRemapTbl() != SBN_SUCCESS) {
+        EVSSendErr(BaseEID, "unable to unload table");
+        return SBN_ERROR;
+    }
+
+    if (OS_MutSemGive(RemapMutex) != OS_SUCCESS)
+    {
+        EVSSendErr(BaseEID, "unable to give mutex");
+        return SBN_ERROR;
+    } /* end if */
+
+    if (RemapMutex != 0)
+    {
+        if(OS_MutSemDelete(RemapMutex) != OS_SUCCESS) {
+            EVSSendErr(BaseEID, "unable to delete mutex");
+            return SBN_ERROR;
+        }
+        RemapMutex = 0;
+    }
+
+    RemapInitialized = false;
+
+    return SBN_SUCCESS;
+}
+
+static SBN_Status_t Init(int Version, CFE_EVS_EventID_t BaseEID)
+{
+    CFE_ES_TaskInfo_t TaskInfo;
+    OS_Status_t OS_Status;
+    CFE_Status_t CFE_Status;
+
+    SBN_F_REMAP_FIRST_EID = BaseEID;
+
+    if (Version != 2) /* TODO: define */
+    {
+        EVSSendErr(BaseEID, "version mismatch: expected %d, got %d", 1, Version);
+        return SBN_ERROR;
+    } /* end if */
+
+    /* get task name to retrieve table after loading */
+    uint32 TskId = OS_TaskGetId();
+    if ((CFE_Status = CFE_ES_GetTaskInfo(&TaskInfo, TskId)) != CFE_SUCCESS)
+    {
+        EVSSendErr(BaseEID, "SBN failed to get task info (%d)", (int)CFE_Status);
+        return SBN_ERROR;
+    } /* end if */
+
+    if(RemapInitialized)
+    {
+        EVSSendErr(BaseEID, "already initialized.");
+        if(Deinit(BaseEID) !=  SBN_SUCCESS) {
+            EVSSendErr(BaseEID, "unable to deinitialize SBN_F_Remap");
+        }
+    }
+
+    OS_Status = OS_MutSemCreate(&RemapMutex, "SBN_F_Remap", 0);
+    if (OS_Status != OS_SUCCESS)
+    {
+        return SBN_ERROR;
+    } /* end if */
+
+    if(LoadRemapTbl() != SBN_SUCCESS) {
+        EVSSendErr(BaseEID, "unable to load remap table");
+    }
+
+    RemapInitialized = true;
+
+    EVSSendInfo(BaseEID, "SBN_F_Remap Lib Initialized.");
+
+    return SBN_SUCCESS;
+} /* end Init() */
+
+SBN_FilterInterface_t SBN_F_Remap = {Init, Remap, Remap, Remap_MID};
---
+++ apps/sbn_udp/fsw/src/sbn_udp_if.c
@@ -0,0 +1,340 @@
+#include "sbn_udp_events.h"
+#include "sbn_udp_if.h"
+#include "sbn_platform_cfg.h"
+#include <string.h>
+#include <errno.h>
+
+#include "sbn_interfaces.h"
+#include "cfe.h"
+
+CFE_EVS_EventID_t SBN_UDP_FIRST_EID;
+
+#define EXP_VERSION 6
+
+static SBN_ProtocolOutlet_t SBN;
+
+static SBN_Status_t Init(int Version, CFE_EVS_EventID_t BaseEID, SBN_ProtocolOutlet_t *Outlet)
+{
+    SBN_UDP_FIRST_EID = BaseEID;
+
+    if (Version != EXP_VERSION)
+    {
+        OS_printf("SBN_UDP version mismatch: expected %d, got %d\n", EXP_VERSION, Version);
+        return SBN_ERROR;
+    } /* end if */
+
+    if (Outlet == NULL)
+    {
+        OS_printf("SBN_UDP outlet is NULL\n");
+        return SBN_ERROR;
+    } /* end if */
+
+    /* copy outlet pointers to a local buffer for later use */
+    memcpy(&SBN, Outlet, sizeof(SBN));
+
+    OS_printf("SBN_UDP Lib Initialized.\n");
+    return SBN_SUCCESS;
+} /* end Init() */
+
+/**
+ * Initializes an UDP host.
+ *
+ * @param  Interface data structure containing the file entry
+ * @return SBN_SUCCESS on success, error code otherwise
+ */
+static SBN_Status_t InitNet(SBN_NetInterface_t *Net)
+{
+    SBN_UDP_Net_t *NetData = (SBN_UDP_Net_t *)Net->ModulePvt;
+
+    OS_SockAddr_t LOCAL_ADDR;
+    uint16 Port;
+    OS_SocketAddrInit(&LOCAL_ADDR, OS_SocketDomain_INET);
+    OS_SocketAddrFromString(&LOCAL_ADDR, "0.0.0.0");
+    OS_SocketAddrGetPort(&Port, &NetData->Addr);
+    OS_SocketAddrSetPort(&LOCAL_ADDR, Port);
+
+    EVSSendInfo(SBN_UDP_SOCK_EID, "creating socket (NetData=0x%lx)", (long unsigned int)NetData);
+
+    if (OS_SocketOpen(&(NetData->Socket), OS_SocketDomain_INET, OS_SocketType_DATAGRAM) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_SOCK_EID, "socket open call failed");
+        return SBN_ERROR;
+    } /* end if */
+
+    if (OS_SocketBind(NetData->Socket, &LOCAL_ADDR) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_SOCK_EID, "bind call failed (NetData=0x%lx, Socket=%d)", (long unsigned int)NetData,
+                   NetData->Socket);
+        return SBN_ERROR;
+    } /* end if */
+
+    return SBN_SUCCESS;
+} /* end InitNet() */
+
+/**
+ * Initializes an UDP host or peer data struct depending on the
+ * CPU name.
+ *
+ * @param  Interface data structure containing the file entry
+ * @return SBN_SUCCESS on success, error code otherwise
+ */
+static SBN_Status_t InitPeer(SBN_PeerInterface_t *Peer)
+{
+    return SBN_SUCCESS;
+} /* end InitPeer() */
+
+static SBN_Status_t ConfAddr(OS_SockAddr_t *Addr, const char *Address)
+{
+    SBN_Status_t Status = SBN_SUCCESS;
+
+    char AddrHost[OS_MAX_API_NAME];
+
+    char *Colon    = strchr(Address, ':');
+    int   ColonLen = 0;
+
+    if (!Colon || (ColonLen = Colon - Address) >= OS_MAX_API_NAME)
+    {
+        EVSSendErr(SBN_UDP_CONFIG_EID, "invalid address (Address=%s)", Address);
+        return SBN_ERROR;
+    } /* end if */
+
+    strncpy(AddrHost, Address, ColonLen);
+    AddrHost[ColonLen] = '\0';
+
+    char *ValidatePtr = NULL;
+    long  Port        = strtol(Colon + 1, &ValidatePtr, 0);
+    if (!ValidatePtr || ValidatePtr == Colon + 1)
+    {
+        EVSSendErr(SBN_UDP_CONFIG_EID, "invalid port (Address=%s)", Address);
+        return SBN_ERROR;
+    } /* end if */
+
+    if ((Status = OS_SocketAddrInit(Addr, OS_SocketDomain_INET)) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_CONFIG_EID, "addr init failed (Status=%d)", Status);
+        return SBN_ERROR;
+    } /* end if */
+
+    EVSSendInfo(SBN_UDP_CONFIG_EID, "addr host: '%s'", AddrHost);
+    if ((Status = OS_SocketAddrFromString(Addr, AddrHost)) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_CONFIG_EID, "addr host set failed (AddrHost=%s, Status=%d)", AddrHost, Status);
+        return SBN_ERROR;
+    } /* end if */
+
+    EVSSendInfo(SBN_UDP_CONFIG_EID, "addr port: '%ld'", Port);
+    if ((Status = OS_SocketAddrSetPort(Addr, Port)) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_CONFIG_EID, "addr port set failed (Port=%ld, Status=%d)", Port, Status);
+        return SBN_ERROR;
+    } /* end if */
+
+    return SBN_SUCCESS;
+} /* end ConfAddr() */
+
+static SBN_Status_t LoadNet(SBN_NetInterface_t *Net, const char *Address)
+{
+    SBN_UDP_Net_t *NetData = (SBN_UDP_Net_t *)Net->ModulePvt;
+
+    EVSSendInfo(SBN_UDP_CONFIG_EID, "configuring net (NetData=0x%lx, Address=%s)", (long unsigned int)NetData, Address);
+
+    SBN_Status_t Status = ConfAddr(&NetData->Addr, Address);
+
+    if (Status == SBN_SUCCESS)
+    {
+        EVSSendInfo(SBN_UDP_CONFIG_EID, "configured (NetData=0x%lx)", (long unsigned int)NetData);
+    } /* end if */
+
+    return Status;
+} /* end LoadNet */
+
+static SBN_Status_t LoadPeer(SBN_PeerInterface_t *Peer, const char *Address)
+{
+    SBN_UDP_Peer_t *PeerData = (SBN_UDP_Peer_t *)Peer->ModulePvt;
+
+    EVSSendInfo(SBN_UDP_CONFIG_EID, "configuring peer (SC=%d, CPU=%d, Address=%s)",
+        Peer->SpacecraftID,
+        Peer->ProcessorID,
+        Address);
+
+    SBN_Status_t Status = ConfAddr(&PeerData->Addr, Address);
+
+    if (Status == SBN_SUCCESS)
+    {
+      EVSSendInfo(SBN_UDP_CONFIG_EID, "configured peer (SC=%d, CPU=%d, Address=%s)",
+          Peer->SpacecraftID,
+          Peer->ProcessorID,
+          Address);
+    } /* end if */
+
+    return Status;
+} /* end LoadPeer() */
+
+static SBN_Status_t PollPeer(SBN_PeerInterface_t *Peer)
+{
+    OS_time_t CurrentTime;
+    OS_GetLocalTime(&CurrentTime);
+
+    EVSSendDbg(SBN_UDP_DEBUG_EID, "polling peer %d:%d", Peer->SpacecraftID, Peer->ProcessorID);
+
+    if (Peer->Connected)
+    {
+        if (OS_TimeGetTotalSeconds(OS_TimeSubtract(CurrentTime, Peer->LastRecv)) > SBN_UDP_PEER_TIMEOUT)
+        {
+            EVSSendInfo(SBN_UDP_DEBUG_EID, "disconnected peer %d:%d", Peer->SpacecraftID, Peer->ProcessorID);
+
+            SBN.Disconnected(Peer);
+            return SBN_SUCCESS;
+        } /* end if */
+
+        if (OS_TimeGetTotalSeconds(OS_TimeSubtract(CurrentTime, Peer->LastSend)) > SBN_UDP_PEER_HEARTBEAT)
+        {
+            OS_GetLocalTime(&Peer->LastSend);
+            EVSSendDbg(SBN_UDP_DEBUG_EID, "sending heartbeat to peer %d:%d", Peer->SpacecraftID, Peer->ProcessorID);
+            return SBN.SendNetMsg(SBN_UDP_HEARTBEAT_MSG, 0, NULL, Peer);
+        } /* end if */
+    }
+    else
+    {
+        if (Peer->ProcessorID != CFE_PSP_GetProcessorId() &&
+            Peer->SpacecraftID != CFE_PSP_GetSpacecraftId() &&
+            OS_TimeGetTotalSeconds(OS_TimeSubtract(CurrentTime, Peer->LastSend)) > SBN_UDP_ANNOUNCE_TIMEOUT)
+        {
+            OS_GetLocalTime(&Peer->LastSend);
+            EVSSendInfo(SBN_UDP_DEBUG_EID, "announce to peer %d:%d", Peer->SpacecraftID, Peer->ProcessorID);
+            return SBN.SendNetMsg(SBN_UDP_ANNOUNCE_MSG, 0, NULL, Peer);
+        } /* end if */
+    }     /* end if */
+
+    return SBN_SUCCESS;
+} /* end PollPeer() */
+
+static SBN_Status_t Send(SBN_PeerInterface_t *Peer, SBN_MsgType_t MsgType, SBN_MsgSz_t MsgSz, void *Payload)
+{
+    int32 BufSz = MsgSz + SBN_PACKED_HDR_SZ, SentSz = 0;
+    uint8 Buf[BufSz];
+
+    SBN_UDP_Peer_t *    PeerData = (SBN_UDP_Peer_t *)Peer->ModulePvt;
+    SBN_NetInterface_t *Net      = Peer->Net;
+    SBN_UDP_Net_t *     NetData  = (SBN_UDP_Net_t *)Net->ModulePvt;
+
+    SBN.PackMsg(Buf, MsgSz, MsgType, CFE_PSP_GetProcessorId(), CFE_PSP_GetSpacecraftId(), Payload);
+
+    OS_SockAddr_t Addr;
+    if (OS_SocketAddrInit(&Addr, OS_SocketDomain_INET) != OS_SUCCESS)
+    {
+        EVSSendErr(SBN_UDP_SOCK_EID, "socket addr init failed");
+        return SBN_ERROR;
+    } /* end if */
+
+    SentSz = OS_SocketSendTo(NetData->Socket, Buf, BufSz, &PeerData->Addr);
+
+    if (SentSz < BufSz)
+    {
+        EVSSendErr(SBN_UDP_SOCK_EID, "incomplete socket send, tried to send %d bytes, returned %d", (int)BufSz,
+                   (int)SentSz);
+        return SBN_ERROR;
+    }
+    else
+    {
+        return SBN_SUCCESS;
+    } /* end if */
+} /* end Send() */
+
+/* Note that this Recv function is indescriminate, packets will be received
+ * from all peers but that's ok, I just inject them into the SB and all is
+ * good!
+ */
+static SBN_Status_t Recv(SBN_NetInterface_t *Net, SBN_MsgType_t *MsgTypePtr, SBN_MsgSz_t *MsgSzPtr,
+                         CFE_ProcessorID_t *ProcessorIDPtr, CFE_SpacecraftID_t *SpacecraftIDPtr, void *Payload)
+{
+    uint8 RecvBuf[SBN_MAX_PACKED_MSG_SZ];
+
+    SBN_UDP_Net_t *NetData = (SBN_UDP_Net_t *)Net->ModulePvt;
+
+    /* task-based peer connections block on reads, otherwise use select */
+
+    uint32 StateFlags = OS_STREAM_STATE_READABLE;
+
+    /* polling uses select, otherwise drop through to block on read for task */
+    if(!(Net->TaskFlags & SBN_TASK_RECV)
+        && (OS_SelectSingle(NetData->Socket, &StateFlags, 0) != OS_SUCCESS
+        || !(StateFlags & OS_STREAM_STATE_READABLE)))
+    {
+        /* nothing to receive */
+        return SBN_IF_EMPTY;
+    }/* end if */
+
+    int Received = OS_SocketRecvFrom(NetData->Socket, (char *)&RecvBuf, CFE_MISSION_SB_MAX_SB_MSG_SIZE, NULL, OS_PEND);
+
+    if (Received < 0)
+    {
+        EVSSendErr(SBN_UDP_DEBUG_EID, "ERROR: could not receive from socket: status=%d", Received);
+        return SBN_ERROR;
+    } /* end if */
+
+    /* each UDP packet is a full SBN message */
+
+    if (SBN.UnpackMsg(&RecvBuf, MsgSzPtr, MsgTypePtr, ProcessorIDPtr, SpacecraftIDPtr, Payload) == false)
+    {
+        EVSSendErr(SBN_UDP_DEBUG_EID, "ERROR: could not unpack message");
+        return SBN_ERROR;
+    } /* end if */
+
+    SBN_PeerInterface_t *Peer = SBN.GetPeer(Net, *ProcessorIDPtr, *SpacecraftIDPtr);
+    if (Peer == NULL)
+    {
+        EVSSendErr(SBN_UDP_DEBUG_EID, "ERROR: unknown peer %d:%d", *SpacecraftIDPtr, *ProcessorIDPtr);
+        return SBN_ERROR;
+    } /* end if */
+
+    if (!Peer->Connected)
+    {
+        EVSSendInfo(SBN_UDP_DEBUG_EID, "connecting to peer %d:%d", *SpacecraftIDPtr, *ProcessorIDPtr);
+        SBN.Connected(Peer);
+    }
+    else
+    {
+        EVSSendDbg(SBN_UDP_DEBUG_EID, "already connected to peer %d:%d", *SpacecraftIDPtr, *ProcessorIDPtr);
+    } /* end if */
+
+    if (*MsgTypePtr == SBN_UDP_DISCONN_MSG)
+    {
+        SBN.Disconnected(Peer);
+    }
+
+    return SBN_SUCCESS;
+} /* end Recv() */
+
+static SBN_Status_t UnloadPeer(SBN_PeerInterface_t *Peer)
+{
+    if (Peer->Connected)
+    {
+        EVSSendInfo(SBN_UDP_DEBUG_EID, "peer %d:%d - sending disconnect", Peer->SpacecraftID, Peer->ProcessorID);
+        SBN.SendNetMsg(SBN_UDP_DISCONN_MSG, 0, NULL, Peer);
+        SBN.Disconnected(Peer);
+    } /* end if */
+
+    return SBN_SUCCESS;
+} /* end UnloadPeer() */
+
+static SBN_Status_t UnloadNet(SBN_NetInterface_t *Net)
+{
+    SBN_Status_t Status = SBN_SUCCESS;
+
+    SBN_PeerIdx_t PeerIdx = 0;
+    for (PeerIdx = 0; PeerIdx < Net->PeerCnt; PeerIdx++)
+    {
+        if(UnloadPeer(&Net->Peers[PeerIdx]) != SBN_SUCCESS) {
+          EVSSendInfo(SBN_UDP_DEBUG_EID, "failed to unload peer: %d\n", PeerIdx);
+	  Status = SBN_ERROR;
+	} else {
+          EVSSendInfo(SBN_UDP_DEBUG_EID, "unloaded peer: %d\n", PeerIdx);
+        }
+    } /* end if */
+
+    return Status;
+} /* end UnloadNet() */
+
+SBN_IfOps_t SBN_UDP_Ops = {Init, InitNet, InitPeer, LoadNet,   LoadPeer,  PollPeer,
+                           Send, NULL,    Recv,     UnloadNet, UnloadPeer};
